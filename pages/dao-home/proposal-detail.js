95e24de69e42ba46e6349f5b0da73a450e0edcb6
lordArora <arorashivam@protonmail.com>
2023-05-04 23:52:57 -0700

    resovle errors 1

--- pages/dao-home/proposal-detail.js
+++ pages/dao-home/proposal-detail.js
@@ -3,8 +3,8 @@ import { useRouter } from "next/router";
 import { connectSocket, socket } from "../../socket";
 import useAuth from "../../hooks/useAuth";
 import Chat from "../../components/chat/Chat";
-import { List, Typography } from "@mui/material";
-import ChatMessage from "../../components/chat/ChatMessage";
+import { CircularProgress, List, Typography } from "@mui/material";
+// import ChatMessage from "../../components/chat/ChatMessage";
 import { ethers } from "ethers";
 
 const DaoProposalDetail = () => {
@@ -157,117 +157,9 @@ const DaoProposalDetail = () => {
           </div>
         </div>
 
-        {conversationsData != null ? (
-          <>
-            <div className="w-full">
-              <h1 className="font-bold text-2xl mt-10">Conversation</h1>
-              <div className="flex-col w-full border mb-10 mt-10">
-                <List
-                  sx={{
-                    height: { xs: "79vh", sm: "79vh", md: "84vh" },
-                    overflowY: "auto",
-                    mt: { xs: 9, sm: 9, md: "20px" },
-                  }}
-                >
-                  <div className="w-full">
-                    {conversationsData != null &&
-                    conversationsData?.[0]?.messages.length > 0 ? (
-                      <>
-                        {conversationsData[0].messages.map((message, id) => {
-                          return (
-                            <ChatMessage
-                              key={id}
-                              message={message.text}
-                              date={message.date}
-                              from={message.from}
-                              sentByMe={
-                                message.from == proposal?.client_address
-                              }
-                              // freelancerData={{
-                              //   ipfsImageHash:
-                              //     "bafkreibwronbp5evijccjhghclp3c6gul4qtl2mb44wajx2dbclapfphia",
-                              // }}
-                              proposalDetailPage={true}
-                            />
-                          );
-                        })}
-                      </>
-                    ) : (
-                      <Typography
-                        component="h5"
-                        variant="h5"
-                        align="center"
-                        mt="40vh"
-                        color="grey.400"
-                      >
-                        This conversation is empty
-                      </Typography>
-                    )}
-                  </div>
-                </List>
-              </div>
-            </div>
-
-            <div>
-              <h1 className="font-bold text-2xl my-5">DAO Comments</h1>
-              {daoMessages && daoMessages.length > 0 ? (
-                daoMessages.map((message) => (
-                  <>
-                    <div className="w-full my-2">
-                      <div className="h-32 border text-center flex items-center justify-center">
-                        <p className="font-light text-xs text-blue-800 p-4">
-                          {" "}
-                          {message?.text}
-                        </p>
-                      </div>
-                      <div className="flex justify-between w-full">
-                        <p className="font-light text-xs text-blue-800 p-4">
-                          {" "}
-                          {message?.from}
-                        </p>
-                        <p className="font-light text-xs text-blue-800 p-4">
-                          {" "}
-                          <div className="flex items-center mt-2.5 mb-5">
-                            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">
-                              {new Date(message.created_at).toLocaleString()}
-                            </span>
-                          </div>
-                        </p>
-                      </div>
-                    </div>
-                  </>
-                ))
-              ) : (
-                <></>
-              )}
-
-              <div class="mb-5">
-                <label
-                  for="large-input"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Write your thoughts
-                </label>
-                <input
-                  type="text"
-                  id="large-input"
-                  class="block w-full p-4 h-64 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500"
-                  onChange={(e) => setMessageState(e.target.value)}
-                  value={messageState}
-                />
-              </div>
-
-              <button
-                class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mb-20"
-                onClick={() => sendMessage()}
-              >
-                Send
-              </button>
-            </div>
-          </>
-        ) : (
-          <div className="min-h-[calc(70vh)] flex items-center justify-center flex-col absoluteCenter">
-            <img
+        {conversationsData == null ? (
+          <div className="flex items-center align-center justify-center flex-col z-50 h-screen">
+            {/* <img
               src={"/empty.png"}
               alt=""
               className="w-1/4 h-1/4"
@@ -276,14 +168,64 @@ const DaoProposalDetail = () => {
               }}
             />
             <p
-              className="text-center text-gray-800 font-bold"
+              className="text-center text-gray-800 font-bold cursor-pointer"
               onClick={() => getConversations()}
             >
-              Please try clicking{" "}
-              <span className="text-blue-700 cursor-pointer"> here </span> or
-              refreshing if you don't see the conversation history
-            </p>
+              Please try clicking <span className="text-blue-700"> here </span>{" "}
+              or refreshing if you don't see your conversations
+            </p> */}
+            {/* <button onClick={() => getConversations()}>Get Data</button> */}
+            <CircularProgress />
           </div>
+        ) : (
+          <>
+            <div className="p-4 sm:ml-80 mt-16">
+              <div className="inline-flex items-center p-2 text-sm rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <button type="button" onClick={() => toggleChatList()}>
+                  <span className="sr-only">Your Chat</span>
+                  <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      fillRule="evenodd"
+                      d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                    ></path>
+                  </svg>
+                </button>
+                <h3 className="p-2 font-bold">Your Chats</h3>
+              </div>
+              {selected != null ? (
+                <Chat
+                  selected={selected}
+                  conversations={
+                    conversationsData.filter((c) => c._id == selected)[0]
+                      .messages
+                  }
+                  to={
+                    conversationsData.filter((c) => c._id == selected)[0]
+                      .participants
+                  }
+                  setConversations={setConversations}
+                  freelancerData={
+                    conversationsData.filter((c) => c._id == selected)[0]
+                      .freelancer[0]
+                  }
+                />
+              ) : (
+                <></>
+              )}
+            </div>
+            <NavDrawer
+              conversationsData={conversationsData}
+              setSelected={setSelected}
+              toggleChatList={toggleChatList}
+            />
+          </>
         )}
       </div>
     </div>
